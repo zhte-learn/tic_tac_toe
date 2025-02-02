414c63a3ba2df2f9089cef731106fe6174574829
tz2015-tatiana <tz2015@hw.ac.uk>
2025-02-02 08:45:33 +0400

    created basic structure of the game - Board and the 1st step

---
+++ game.py
@@ -0,0 +1,13 @@
+from gameparts import Board
+
+
+def main():
+    game = Board()
+    game.display()
+    game.make_move(1, 1, 'X')
+    print('Ход сделан!')
+    game.display()
+
+
+if __name__ == '__main__':
+    main()
